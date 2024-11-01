fdf6b53ded1d06805747fdcbaff640767b4d961d
Justin Maronde <myemailisjustin@me.com>
2024-11-01 05:50:52 -0400

    create a two column NavigationView layout with SideBar and MapScreen as the detail placeholders.

---
+++ JustinMaps/Screens/SideBar.swift
@@ -0,0 +1,18 @@
+//
+//  SideBar.swift
+//  JustinMaps
+//
+//  Created by Justin Maronde on 11/1/24.
+//
+
+import SwiftUI
+
+struct SideBar: View {
+    var body: some View {
+        Text("Sidebar Placeholder")
+    }
+}
+
+#Preview {
+    SideBar()
+}
