a69968a44d8e7274a28016714d4410849f020604
Justin Maronde <myemailisjustin@me.com>
2024-11-01 05:39:05 -0400

    Initial Commit

---
+++ JustinMaps/JustinMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustinMapsApp.swift
+//  JustinMaps
+//
+//  Created by Justin Maronde on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct JustinMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
