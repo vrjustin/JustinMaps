de260786c2b8927b743f7e142674f646845f7312
Justin Maronde <myemailisjustin@me.com>
2024-11-02 06:00:55 -0400

    display a flyover image of the selected PlaceAnnotation.

--- JustinMaps/Views/Callouts/PlaceCalloutView.swift
+++ JustinMaps/Views/Callouts/PlaceCalloutView.swift
@@ -55,6 +55,22 @@ class PlaceCalloutView: NSView {
         stackView.orientation = .vertical
         stackView.edgeInsets = .init(top: 0, left: 20, bottom: 0, right: 20)
         
+        let options = MKMapSnapshotter.Options()
+        options.size = CGSize(width: frame.width, height: frame.height / 2)
+        options.mapType = .satelliteFlyover
+        
+        options.camera = MKMapCamera(lookingAtCenter: annotation.coordinate, fromDistance: 50, pitch: 65, heading: 0)
+        
+        let snapshotter = MKMapSnapshotter(options: options)
+        snapshotter.start { snapshot, error in
+            guard let snapshot = snapshot, error == nil else { return }
+            DispatchQueue.main.async {
+                let imageView = NSImageView(frame: NSRect(x: 0, y: 0, width: 100, height: 100))
+                imageView.image = snapshot.image
+                stackView.insertView(imageView, at: 0, in: .top)
+            }
+        }
+
         stackView.addArrangedSubview(directionsButton)
         stackView.addArrangedSubview(phoneTextField)
         stackView.addArrangedSubview(addressTextField)
