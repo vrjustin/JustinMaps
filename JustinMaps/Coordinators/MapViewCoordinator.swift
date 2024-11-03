edbd64a987cc3a30ac5630819821fdd1f8b0d848
Justin Maronde <myemailisjustin@me.com>
2024-11-03 05:21:52 -0500

    fire the closure selectShowDirections when the GetDirections button is pressed. create function that takes in source and destination MKMapItems and generates directions request. print the steps out.

--- JustinMaps/Coordinators/MapViewCoordinator.swift
+++ JustinMaps/Coordinators/MapViewCoordinator.swift
@@ -19,7 +19,42 @@ final class MapViewCoordinator: NSObject, MKMapViewDelegate {
         guard let annotation = view.annotation as? PlaceAnnotation else { return }
         
         view.canShowCallout = true
-        view.detailCalloutAccessoryView = PlaceCalloutView(annotation: annotation)
+        view.detailCalloutAccessoryView = PlaceCalloutView(annotation: annotation, selectShowDirections: { [weak self] placeAnnotation in
+            // show directions to the place in the placeAnnotation
+            let start = MKMapItem.forCurrentLocation()
+            let destination = MKMapItem(placemark: MKPlacemark(coordinate: placeAnnotation.coordinate))
+            
+            self?.calculateRoute(start: start, destination: destination)
+        })
+    }
+    
+    func calculateRoute(start: MKMapItem, destination: MKMapItem) {
+        
+        let directionsRequest = MKDirections.Request()
+        directionsRequest.transportType = .automobile
+        directionsRequest.source = start
+        directionsRequest.destination = destination
+        
+        let directions = MKDirections(request: directionsRequest)
+        directions.calculate { response, error in
+            
+            if let error = error {
+                print("Unable to calculate directions: \(error)")
+                return
+            }
+            
+            guard let response = response,
+                let route = response.routes.first else {
+                    return
+            }
+            
+            for step in route.steps {
+                print(step.instructions)
+            }
+            
+            
+        }
+        
     }
     
 }
