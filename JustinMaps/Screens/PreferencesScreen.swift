cf51332ecfc4566f6301d449c5ae99267cc938eb
Justin Maronde <myemailisjustin@me.com>
2024-11-04 06:21:25 -0500

    Generate a PreferencesScreen Generate a new commandGroup visible after the appInfo in the app about menu. Add view extension that allows a view to be opened in a new window.

---
+++ JustinMaps/Screens/PreferencesScreen.swift
@@ -0,0 +1,22 @@
+//
+//  PreferencesScreen.swift
+//  JustinMaps
+//
+//  Created by Justin Maronde on 11/4/24.
+//
+
+import SwiftUI
+
+struct PreferencesScreen: View {
+    var body: some View {
+        VStack {
+            Text("Hello Preferences Screen")
+        }
+        .padding()
+        .frame(minWidth: 400, minHeight: 400)
+    }
+}
+
+#Preview {
+    PreferencesScreen()
+}
