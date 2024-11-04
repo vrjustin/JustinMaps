57b843b0ff029155942a91c401cd988e93323bb7
Justin Maronde <myemailisjustin@me.com>
2024-11-04 08:36:53 -0500

    Use AppStorage to store particular settings/preferences values. Generate UI to toggle and select the values from AppStorage on the PreferencesScreen.

--- JustinMaps/Screens/PreferencesScreen.swift
+++ JustinMaps/Screens/PreferencesScreen.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct PreferencesScreen: View {
+    
+    @AppStorage("useLightMap") var useLightMap: Bool = false
+    @AppStorage("distanceUnit") var distanceUnit = DistanceUnit.miles
+    
+    
     var body: some View {
-        VStack {
-            Text("Hello Preferences Screen")
+        VStack(alignment: .center, spacing: 20) {
+            Toggle(isOn: $useLightMap) {
+                Text("Always use light map appearance")
+            }
+            
+            Divider()
+            
+            HStack {
+                Text("Distance Units")
+                Picker("", selection: $distanceUnit) {
+                    ForEach(DistanceUnit.allCases, id: \.self) { distanceUnit in
+                        Text(distanceUnit.title)
+                    }
+                }
+                .fixedSize()
+                .padding(.trailing, 20)
+                
+            }
         }
         .padding()
         .frame(minWidth: 400, minHeight: 400)
